eaf5f568e9b4c8cb82b399e54c899cc0d2082ff7
yasas-mintpay <108509570+yasas-mintpay@users.noreply.github.com>
2022-09-02 19:02:37 +0530

    feat(animal): all animal apis

--- controllers/animal.js
+++ controllers/animal.js
@@ -72,9 +72,109 @@ export const addAnimal = async (req, res) => {
 
 export const getAnimalsList = async (req, res) => {
   await Animal.find().then((animals) => {
-    
+
     return res.status(200).json({
       message: 'Animals List Fetched Successfully',
-      animalsList: animals
+      animalsList: animals,
     })
-})}
+  })
+}
+
+export const getSingleAnimal = async (req, res) => {
+  const { id } = req.query
+
+  await Animal.findById(id).then((animal) => {
+    return res.status(200).json({
+      message: 'Animal Fetched Successfully',
+      animal: animal,
+    })
+  })
+}
+
+export const deleteSingleAnimal = async (req, res) => {
+  const { id } = req.body
+
+  await Animal.findByIdAndDelete(id).then((animal) => {
+    return res.status(200).json({
+      message: 'Animal Deleted Successfully',
+      animal: animal,
+    })
+  }).catch((err) => {
+    return res.status(500).json({
+      message: 'Error deleting animal',
+      err,
+    })
+  })
+
+}
+
+export const editSingleAnimal = async (req, res) => {
+  const {
+    id,
+    nameInput,
+    scientificNameInput,
+    tagInput,
+    descriptionInput,
+    avgMaleWeightInput,
+    avgFemaleWeightInput,
+    avgMaleHeightInput,
+    avgFemaleHeightInput,
+    kingdomOfAnimal,
+    phylumOfAnimal,
+    classOfAnimal,
+    orderOfAnimal,
+    familyOfAnimal,
+    genusOfAnimal,
+    speciesOfAnimal,
+    userId,
+  } = req.body
+
+
+  if (!nameInput || !scientificNameInput || !tagInput || !descriptionInput || !avgMaleWeightInput || !avgFemaleWeightInput || !avgMaleHeightInput || !avgFemaleHeightInput || !kingdomOfAnimal || !phylumOfAnimal || !classOfAnimal || !orderOfAnimal || !familyOfAnimal || !genusOfAnimal || !speciesOfAnimal) {
+    return res.status(400).json({
+      message: 'Please fill all fields',
+    })
+  }
+
+  const { _id } = await User.findById(userId)
+
+  if (!_id) {
+    return res.status(400).json({
+      message: 'User not found',
+    })
+  }
+
+  await Animal.findOneAndUpdate(
+    {
+      _id: id,
+      publisherId: _id,
+    },
+    {
+      nameInput,
+      scientificNameInput,
+      tagInput: tagInput,
+      descriptionInput,
+      avgMaleWeightInput,
+      avgFemaleWeightInput,
+      avgMaleHeightInput,
+      avgFemaleHeightInput,
+      kingdomOfAnimal,
+      phylumOfAnimal,
+      classOfAnimal,
+      orderOfAnimal,
+      familyOfAnimal,
+      genusOfAnimal,
+      speciesOfAnimal,
+    }).then((createdAnimal) => {
+    return res.status(200).json({
+      message: 'Animal updated successfully',
+      animal: createdAnimal,
+    })
+  }).catch((err) => {
+    return res.status(500).json({
+      message: 'Error updating animal',
+      err,
+    })
+  })
+
+}
