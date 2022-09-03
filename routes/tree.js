ceb073a7b4776dec4730c1b7e9a75ef27527e487
kethmi <it20021320@my.sliit.lk>
2022-09-03 13:19:48 +0530

    feat(tree model) : update tree object

--- routes/tree.js
+++ routes/tree.js
@@ -1,5 +1,12 @@
 import express, { Router } from 'express'
-import { CreateTree, retrieveAll, retriveUserPosts , deletePost } from '../controllers/tree'
+import {
+  CreateTree,
+  retrieveAll,
+  retriveUserPosts,
+  deletePost,
+  retrieveOneUpdate,
+  updatePost,
+} from '../controllers/tree'
 import { checkUser } from '../middlewares/auth'
 
 const router = express.Router()
@@ -7,6 +14,8 @@ const router = express.Router()
 router.post('/tree', checkUser, CreateTree)
 router.get('/gettrees', retrieveAll)
 router.get('/getuserposts', checkUser, retriveUserPosts)
-router.delete('/deleteTree' , checkUser , deletePost )
+router.delete('/deleteTree', checkUser, deletePost)
+router.delete('/getOneUpdate', checkUser, retrieveOneUpdate)
+router.put('/updatepost', checkUser, updatePost)
 
 module.exports = router
