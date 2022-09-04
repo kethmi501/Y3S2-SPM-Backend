9923bc2701be81bb86cc313839401e46076fcdeb
Sameera <sameeranicho@gmail.com>
2022-09-04 15:21:47 +0530

    view locations

--- routes/location.js
+++ routes/location.js
@@ -1,9 +1,12 @@
 import express from 'express'
 import { checkUser } from '../middlewares/auth'
-import { createLocation } from '../controllers/location'
+import { createLocation, retrieveAll, likePost , retrieveUserPosts } from '../controllers/location'
 
 const router = express.Router()
 
 router.post('/location/addlocation', checkUser, createLocation)
+router.get('/location/retrieveall', checkUser, retrieveAll)
+router.get('/location/retrieveuserposts', checkUser, retrieveUserPosts)
+router.put('/locaton/likePost', checkUser, likePost)
 
 module.exports = router
