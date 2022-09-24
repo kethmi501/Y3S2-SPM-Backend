290e82197e686d47f55a3d9c5741329f60fd71a5
Sameera <sameeranicho@gmail.com>
2022-09-24 13:49:44 +0530

    delete function

--- controllers/location.js
+++ controllers/location.js
@@ -46,15 +46,14 @@ export const likePost = async (req, res) => {
     .then((res) => {
       likesArr = res.likes
       const index = likesArr.findIndex((object) => object === userId)
-      if (index === -1) {
-        likesArr.push(userId)
-      }
-      if (index === 0) {
+      if (index === 1) {
         const idx = likesArr.indexOf(userId)
         if (idx > -1) {
           // only splice array when item is found
           likesArr.splice(idx, 1) // 2nd parameter means remove one item only
         }
+      } else if (index === -1) {
+        likesArr.push(userId)
       }
     })
     .then(async () => {
@@ -72,5 +71,27 @@ export const likePost = async (req, res) => {
         .catch((err) => {
           res.status(500).json(err)
         })
+    })
+}
+
+export const deletePost = async (req, res) => {
+  const { userId, postId } = req.body
+  await Location.deleteOne({ publisherId: userId, _id: postId })
+    .then((result) => {
+      res.status(200).json(result)
+    })
+    .catch((err) => {
+      res.status(500).json(err)
+    })
+}
+
+export const retrieveOneUpdate = async (req, res) => {
+  const { userId, postId } = req.body
+  await Location.findOne({ publisherId: userId, _id: postId })
+    .then((result) => {
+      res.status(200).json(result)
+    })
+    .catch((err) => {
+      res.status(500).json(err)
     })
 }
--- routes/location.js
+++ routes/location.js
@@ -1,12 +1,14 @@
 import express from 'express'
 import { checkUser } from '../middlewares/auth'
-import { createLocation, retrieveAll, likePost , retrieveUserPosts } from '../controllers/location'
+import { createLocation, retrieveAll, likePost , retrieveUserPosts , deletePost , retrieveOneUpdate } from '../controllers/location'
 
 const router = express.Router()
 
 router.post('/location/addlocation', checkUser, createLocation)
 router.get('/location/retrieveall', checkUser, retrieveAll)
+router.post('/location/retrieveOneUpdate', checkUser, retrieveOneUpdate)
 router.get('/location/retrieveuserposts', checkUser, retrieveUserPosts)
 router.put('/locaton/likePost', checkUser, likePost)
+router.delete('/location/delete',checkUser, deletePost)
 
 module.exports = router
