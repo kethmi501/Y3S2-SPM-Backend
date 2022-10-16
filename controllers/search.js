6f44632edbb4f075c8d6e23fa925edbd3ea1f0a9
Osini Kithma <66475844+swee76@users.noreply.github.com>
2022-10-16 16:04:24 +0530

    animal list page bug fix

--- controllers/search.js
+++ controllers/search.js
@@ -27,3 +27,16 @@ const entities = async (req, res) => {
 
 
 }
+
+const entities5 = async (req, res) => {
+  const { query } = req.query
+
+  const animalResults = await Animal.find({ nameInput: { $regex: query, $options: 'i' } })
+  const treeResults = await Tree.find({ name: { $regex: query, $options: 'i' } })
+  const LocationResults = await Location.find({ address: { $regex: query, $options: 'i' } })
+
+
+  res.status(200).json({ animalResults, treeResults, LocationResults })
+
+
+}
